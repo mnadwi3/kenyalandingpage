679ecbaa5225fed2d7ee56963869f599377ed99d
Mohd Mohsin <mnadwi3@gmail.com>
2026-08-07 22:36:38 +0530

    Rename doctors section heading to "Top Doctors for {treatment name}"

--- adminpanel/views/public/treatments/show.php
+++ adminpanel/views/public/treatments/show.php
@@ -194,7 +194,7 @@ $secHead = static function (string $title): string {
 
 <section class="tp-section" id="doctors">
   <div class="wrap">
-    <?= $secHead('The Team Reviewing Your Case') ?>
+    <?= $secHead('Top Doctors for ' . $name) ?>
     <div class="docgrid" data-treatment="<?= e($name) ?>" aria-live="polite"></div>
   </div>
 </section>
